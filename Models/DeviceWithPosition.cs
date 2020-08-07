bf9863cb667e5dc894ea1a1c09d72e91a0316710
Milana <milana.bijeljanin@gmail.com>
2020-08-07 14:32:01 +0200

    Add device

--- Models/DeviceWithPosition.cs
+++ Models/DeviceWithPosition.cs
@@ -2,7 +2,6 @@ namespace Electric.Models
 {
     public class DeviceWithPosition
     {
-        public int EnclosureId { set; get; }
         public Device Device { set; get; }
         public int Row { set; get; }
         public int Column { set; get; }
