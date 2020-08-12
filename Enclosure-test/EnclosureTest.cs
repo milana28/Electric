1c448f852564e387e234bb7242625280cebb247b
Milana <milana.bijeljanin@gmail.com>
2020-08-12 16:48:32 +0200

    CheckIfEnclosureSpecsIsAppropriateFalse Test

--- Enclosure-test/EnclosureTest.cs
+++ Enclosure-test/EnclosureTest.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using Electric.Models;
 using Xunit;
 using Enclosure = Electric.Domain.Enclosure;
 
-namespace Enclosure_test
+namespace Electric
 {
     public class EnclosureTest
     {
